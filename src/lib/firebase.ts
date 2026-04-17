6d646e7eef2e7cef2759d74eb7bb7e78a7ed2acb
Renan Matheus <rwnanmatheus@gmail.com>
2026-04-17 14:33:43 -0300

    fix: firebase auth and firestore exports

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "COLE_AQUI",
-  authDomain: "COLE_AQUI",
-  projectId: "COLE_AQUI",
-  storageBucket: "COLE_AQUI",
-  messagingSenderId: "COLE_AQUI",
-  appId: "COLE_AQUI",
+  apiKey: "AIzaSyBHX51OzHeliEuhUt86V-UxUFWhjqNWPBM",
+  authDomain: "tgpi-next.firebaseapp.com",
+  projectId: "tgpi-next",
+  storageBucket: "tgpi-next.firebasestorage.app",
+  messagingSenderId: "420470087490",
+  appId: "1:420470087490:web:080e29fbc61255d03172db",
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
