f7302f797778b0589af0d8b1f58a1c96dd61c1b2
RENAN MATHEUS <rwnanmatheus@gmail.com>
2026-08-03 18:52:58 -0300

    feat(types): export canonical TGPI contracts

---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+export type * from "./billing";
+export type * from "./certificate";
+export type * from "./country";
+export type * from "./notification";
+export type * from "./profile";
+export type * from "./ranking";
+export type * from "./recommendation";
+export type * from "./user";
